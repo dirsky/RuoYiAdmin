232056347294600e235748d6c50c272c179d28d7
Frank <dirsky@126.com>
2019-09-10 15:18:32 +0800

    初步修改了部分内容

--- ruoyi-admin/src/main/java/com/ruoyi/web/core/config/SwaggerConfig.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/core/config/SwaggerConfig.java
@@ -50,13 +50,13 @@ public class SwaggerConfig
         // 用ApiInfoBuilder进行定制
         return new ApiInfoBuilder()
                 // 设置标题
-                .title("标题：若依管理系统_接口文档")
-                // 描述
-                .description("描述：用于管理集团旗下公司的人员信息,具体包括XXX,XXX模块...")
-                // 作者信息
-                .contact(new Contact(Global.getName(), null, null))
+                .title("标题：后台管理系统_接口文档")
                 // 版本
                 .version("版本号:" + Global.getVersion())
+                // 描述
+                .description("描述：用于管理对外的接口信息")
+                // 作者信息
+                .contact(new Contact(Global.getName(), null, null))
                 .build();
     }
 }
